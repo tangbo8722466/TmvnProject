bcf2fc4e97ae1f63cd88f8fac6f697c8c8d5155c
tangbo <617192504@qq.com>
2016-04-16 14:06:43 +0800

    add file

---
+++ src/main/java/com/bobo/test/TmvnProject/App.java
@@ -0,0 +1,13 @@
+package com.bobo.test.TmvnProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
